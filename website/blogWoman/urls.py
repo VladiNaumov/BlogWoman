6a26fbed44bf3c4e80b26b3a324d0f1964ec0afe
Sim <https://github.com/VladiNaumov>
2022-03-30 20:14:55 +0300

    Web Blog woman

--- website/blogWoman/urls.py
+++ website/blogWoman/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, re_path
-from .views import index, categories, archive, about, phones
+from .views import index, about, phones
 
 urlpatterns = [
     path('', index, name = 'home'), # http://127.0.0.1:8000/
     path('about/', about, name = 'about'), #http://127.0.0.1:8000/about
-    path('phones/', phones)
+    path('phones/', phones, name = 'phones' )
 ]
 
--- website/blogWoman/views.py
+++ website/blogWoman/views.py
@@ -1,28 +1,22 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 
+from blogWoman.models import Women, Phones
+
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def index(request):
-    return render(request, 'women/index.html', {'menu': menu, 'title': 'index page'})
+    posts = Women.objects.all()
+    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'index page'})
+
+
+def phones(request):
+    posts = Phones.objects.all()
+    return render(request, 'women/phones.html', {'posts': posts, 'menu': menu, 'title': 'Phones'})
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'Abaut'})
 
-def phones(request):
-    return HttpResponse('<h1>PHONES</h1>')
-
-def categories(request, catid):
-    if request.POST:
-        print(request.POST)
-
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
-
-def archive(request, year):
-    if int(year) > 2022:
-        return redirect('home', permanent=False) # permanent=False->redirect 302, permanent=True -> 301
-
-    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
