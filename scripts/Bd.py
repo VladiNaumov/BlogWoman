6afb36ea29966c19e9e2b291b34de1987ced3983
Sim <https://github.com/VladiNaumov>
2022-04-04 18:17:10 +0300

    MyBlog

--- scripts/Bd.py
+++ scripts/Bd.py
@@ -3,22 +3,62 @@ import sqlite3
 """ пример программы, которая создает новую базу данных перед открытием ее для операций: """
 
 db_filename = '../website/orders.db'
+#db_filename = '../website/db.sqlite3'
 connection = sqlite3.connect(db_filename)
 cur = connection.cursor()
 
 """ создание таблице user """
-cur.execute("""CREATE TABLE IF NOT EXISTS users(
-   userid INT PRIMARY KEY,
-   fname TEXT,
-   lname TEXT,
-   gender TEXT);
-""")
-connection.commit()
+#cur.execute("""CREATE TABLE IF NOT EXISTS users(
+#   userid INT PRIMARY KEY,
+#   fname TEXT,
+#   lname TEXT,
+#   gender TEXT);
+#""")
+#connection.commit()
 
 """ заполнаяем таблицу """
-cur.execute("""INSERT INTO users(userid, fname, lname, gender) 
-   VALUES('00090', 'Alex', 'Smith', 'male');""")
+#cur.execute("""INSERT INTO users(userid, fname, lname, gender)
+#   VALUES('00090', 'Alex', 'Smith', 'male');""")
+#connection.commit()
+
+#more_users = [('00003', 'Peter', 'Parker', 'Male'), ('00004', 'Bruce', 'Wayne', 'male')]
+
+"""
+customers = [
+  ('00005', 'Stephanie', 'Stewart', 'female'), ('00006', 'Sincere', 'Sherman', 'female'), ('00007', 'Sidney', 'Horn', 'male'),
+  ('00008', 'Litzy', 'Yates', 'female'), ('00009', 'Jaxon', 'Mills', 'male'), ('00010', 'Paul', 'Richard', 'male'),
+  ('00011', 'Kamari', 'Holden', 'female'), ('00012', 'Gaige', 'Summers', 'female'), ('00013', 'Andrea', 'Snow', 'female'),
+  ('00014', 'Angelica', 'Barnes', 'female'), ('00015', 'Leah', 'Pitts', 'female'), ('00016', 'Dillan', 'Olsen', 'male'),
+  ('00017', 'Joe', 'Walsh', 'male'), ('00018', 'Reagan', 'Cooper', 'male'), ('00019', 'Aubree', 'Hogan', 'female'),
+  ('00020', 'Avery', 'Floyd', 'male'), ('00021', 'Elianna', 'Simmons', 'female'), ('00022', 'Rodney', 'Stout', 'male'),
+  ('00023', 'Elaine', 'Mcintosh', 'female'), ('00024', 'Myla', 'Mckenzie', 'female'), ('00025', 'Alijah', 'Horn', 'female'),
+  ('00026', 'Rohan', 'Peterson', 'male'), ('00027', 'Irene', 'Walters', 'female'), ('00028', 'Lilia', 'Sellers', 'female'),
+  ('00029', 'Perla', 'Jefferson', 'female'), ('00030', 'Ashley', 'Klein', 'female')
+]
+
+cur.executemany("INSERT INTO users VALUES(?, ?, ?, ?);", customers)
 connection.commit()
+"""
+
+""" Получение данных с SQLite в Python """
+
+"""
+cur.execute("SELECT * FROM users;")
+one_result = cur.fetchone()
+print(one_result)
+
+# скрипт для генерации 3 результатов
+cur.execute("SELECT * FROM users;")
+three_results = cur.fetchmany(3)
+print(three_results)
+
+
+"""
+
+# Функцию fetchall() можно использовать для получения всех результатов.
+cur.execute("SELECT * FROM users;")
+all_results = cur.fetchall()
+print(all_results)
 
 connection.close()
 
