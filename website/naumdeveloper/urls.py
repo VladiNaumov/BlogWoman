819e12962b41f1d7026477c92d38cda4ceb52e4c
Sim <https://github.com/VladiNaumov>
2022-04-06 18:09:09 +0300

    MyBlog

--- website/naumdeveloper/urls.py
+++ website/naumdeveloper/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from naumdeveloper.views import index, store, email
 
 urlpatterns = [
-    path('', index, name = 'home'), # http://127.0.0.1:8000/naumdeveloper
+    path('', index, name = 'index'), # http://127.0.0.1:8000/naumdeveloper
     path('email/', email, name = 'email'), # http://127.0.0.1:8000/naumdeveloper/email
     path('store/', store, name = 'store' )
 ]
