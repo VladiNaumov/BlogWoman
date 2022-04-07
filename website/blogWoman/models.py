99bce7b5862b5eadd7acf845f01e14bd847a88c5
Sim <https://github.com/VladiNaumov>
2022-04-07 20:30:11 +0300

    MyBlog

--- website/blogWoman/models.py
+++ website/blogWoman/models.py
@@ -13,6 +13,7 @@ class Women(models.Model):
     def __str__(self):
         return self.title
 
+    # функция для формирования ссылки для постов
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
